275f49f231bea30c8ed54ba5578c4468e0bff319
Dmitry Pukhov <pukhov@gnivc.ru>
2022-05-08 02:48:07 +0300

    BinanceFeed last candle time fixed

--- biml/feed/BinanceFeed.py
+++ biml/feed/BinanceFeed.py
@@ -2,11 +2,8 @@ import logging
 import time
 from datetime import timedelta
 from typing import List
-
-import numpy as np
 import pandas as pd
 from binance.spot import Spot as Client
-
 from biml.feed.BaseFeed import BaseFeed
 from biml.feed.TickerInfo import TickerInfo
 
@@ -19,10 +16,6 @@ class BinanceFeed(BaseFeed):
     def __init__(self, spot_client: Client, tickers: List[TickerInfo]):
         super().__init__(tickers)
         self.spot_client: Client = spot_client
-
-        # Read interval is a minimum candle interval
-        # self.read_interval = min([pd.to_timedelta(interval) for interval in
-        #                           itertools.chain([t.candle_intervals for t in self.tickers])])
         self.read_interval = timedelta(minutes=1)
 
     def run(self):
@@ -49,8 +42,6 @@ class BinanceFeed(BaseFeed):
         logging.debug(f"Got binance server time {binance_time}")
         for ticker in self.tickers:
             for interval in ticker.candle_intervals:
-                # Reset new candles
-                # ticker.new_candles = {}
                 if ticker.candle_last_times[interval] \
                         and (binance_time - ticker.candle_last_times[interval]) < pd.to_timedelta(interval):
                     # Time not elapsed for this interval, i.e. 15 minutes should pass from last time for M15
@@ -59,7 +50,7 @@ class BinanceFeed(BaseFeed):
                 # Prepare params
                 limit = ticker.candle_limits[interval]
                 start_time = ticker.candle_last_times[interval]
-                start_time_millis = start_time.to_datetime64() if start_time else None
+                start_time_millis = start_time.value//10**6 if start_time else None
 
                 logging.debug(f"Read data from binance. ticker={ticker.ticker}, interval={interval}, "
                               f"start_time={start_time}, start_time_millis={start_time_millis} limit={limit}")
@@ -78,6 +69,6 @@ class BinanceFeed(BaseFeed):
                         'close_time'].max(), unit='ms')
                 # Convert raw data to candles df
                 new_candles = self.preprocess(new_candles)
-                # Produce on_candles event if present
+                # Produce on_candles event
                 for consumer in [c for c in self.consumers if hasattr(c, 'on_candles')]:
                     consumer.on_candles(ticker=ticker.ticker, interval=interval, candles=new_candles)
