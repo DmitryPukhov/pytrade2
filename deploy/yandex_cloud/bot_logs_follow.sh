ffc3551e3ffa3b73f9608af692fa24d85a58f667
Dmitry Pukhov <pukhov@gnivc.ru>
2023-05-10 23:21:04 +0300

    bots scripts renamed

---
+++ deploy/yandex_cloud/bot_logs_follow.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bot_name=${1?"Please provide bot name.\n Example: $0 lstm"} # arg1 is a bot name
+
+user="yc-user"
+public_ip="$(yc compute instance list | grep biml-trade-bots | awk '{print $10}')"
+
+ssh $user@"$public_ip" "sudo docker logs -f biml-$bot_name &"
