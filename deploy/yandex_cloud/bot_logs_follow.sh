92c36a4311737ac4778a0d0fef9c45cbf01560dd
Dmitry Pukhov <pukhov@gnivc.ru>
2023-05-11 10:57:35 +0300

    bot_logs_follow.sh set n=100

--- deploy/yandex_cloud/bot_logs_follow.sh
+++ deploy/yandex_cloud/bot_logs_follow.sh
@@ -5,4 +5,4 @@ bot_name=${1?"Please provide bot name.\n Example: $0 lstm"} # arg1 is a bot name
 user="yc-user"
 public_ip="$(yc compute instance list | grep biml-trade-bots | awk '{print $10}')"
 
-ssh $user@"$public_ip" "sudo docker logs -f biml-$bot_name &"
+ssh $user@"$public_ip" "sudo docker logs -n 100 -f biml-$bot_name &"
