0121e1823a0617bff753c0ea37e266726fdde542
Dmitry Pukhov <pukhov@gnivc.ru>
2023-05-12 21:23:10 +0300

    Static ip assigned to vm

--- deploy/yandex_cloud/vm_create.sh
+++ deploy/yandex_cloud/vm_create.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-vm_name=biml-trade-bots
+app_name=biml-trade-bots
+zone="ru-central1-a"
 
 create_instance() {
-  echo "Creating virtual machine $vm_name"
+  echo "Creating virtual machine $app_name"
   # standard-v2 cascade lake or standard-v3 ice lake
   yc compute instance create \
-    --name $vm_name \
-    --hostname $vm_name \
-    --zone ru-central1-a \
+    --name $app_name \
+    --hostname $app_name \
+    --zone $zone\
     --memory 4 \
     --cores 2 \
     --platform "standard-v3" \
-    --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
-    --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-2204-lts,type=network-ssd,size=32 \
+    --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4,nat-address="$public_ip" \
+    --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-2204-lts,type=network-hdd,size=32 \
     --ssh-key ./secret/id_rsa.pub
 }
 
-install_docker() {
+install_prereq() {
   user="yc-user"
   echo "Install prerequisites to $public_ip"
   ssh "$user"@"$public_ip" "sudo snap install python38"
@@ -28,9 +29,16 @@ install_baremetal() {
   ssh "$user"@"$public_ip" "sudo snap install python38"
 }
 
-# Create instance with docker
+# Main
+
+# Static ip
+yc vpc address create --name $app_name --external-ipv4 zone=$zone
+public_ip=$(yc vpc address list | grep $app_name | awk '{print $6}')
+
+# Vm
 create_instance
-public_ip="$(yc compute instance list | grep $vm_name | awk '{print $10}')"
-install_docker
+
+# Docker
+install_prereq
 
 
--- deploy/yandex_cloud/vm_delete.sh
+++ deploy/yandex_cloud/vm_delete.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-vm_name=biml-trade-bots
-echo "Stopping biml vm $vm_name"
-yc compute instance stop --name $vm_name || true
-echo "Deleting biml vm"
-yc compute instance delete --name $vm_name
+app_name=biml-trade-bots
+
+echo "Stopping biml vm $app_name"
+yc compute instance stop --name $app_name || true
+
+echo "Deleting biml vm $app_name"
+yc compute instance delete --name $app_name
+
+echo "Deleting static ip for $app_name"
+yc vpc address delete $app_name
