f8eae2963fa502f616408bc85d4e9b1bf15c4cc1
Dmitry Pukhov <pukhov@gnivc.ru>
2023-05-19 07:26:22 +0300

    Added broker unit test for main order not filled case.

--- test/broker/test_BinanceBroker.py
+++ test/broker/test_BinanceBroker.py
@@ -16,13 +16,35 @@ class TestBinanceBroker(unittest.TestCase):
 
     def setUp(self) -> None:
         mock.patch.object(BinanceBroker, '__init_db__', self.__broker__init_db__).start()
+        mock.patch.object(BinanceBroker, "read_last_opened_trade", lambda self: None).start()
 
-    def test_create_cur_trade(self):
+    def test_create_cur_trade__not_filled(self):
+        # Binance client mock
+        client = MagicMock()
+        client.new_order = Mock(return_value={"status": "bad"})
+        client.new_oco_order = Mock()
+
+        # Class under test
+        broker = BinanceBroker(client, TestBinanceBroker.config)
+
+        # Test call
+        broker.create_cur_trade(symbol="BTCUSDT",
+                                direction=1,
+                                quantity=1.0,
+                                price=10,
+                                stop_loss_price=9,
+                                take_profit_price=11)
+
+        # Assert
+        client.new_order.assert_called_once()  # Main order attempt
+        client.new_oco_order.assert_not_called()  # No sl/tp call if main order failed
+
+    def test_create_cur_trade__sl_tp_from_filled_price(self):
         # Binance client mock
         client = Mock()
         client.new_order = Mock(return_value={"status": "FILLED",
                                               "orderId": 1,
-                                              "fills": [{"price": 10}],
+                                              "fills": [{"price": 20}],
                                               "transactTime": 123})
         client.new_oco_order = Mock(return_value={"status": "FILLED", "orderListId": 2})
 
@@ -43,9 +65,9 @@ class TestBinanceBroker(unittest.TestCase):
 
         self.assertEqual("BUY", broker.cur_trade.side)
         self.assertEqual(1.0, broker.cur_trade.quantity)
-        self.assertEqual(10, broker.cur_trade.open_price)
-        self.assertEqual(9, broker.cur_trade.stop_loss_price)
-        self.assertEqual(11, broker.cur_trade.take_profit_price)
+        self.assertEqual(20, broker.cur_trade.open_price)
+        self.assertEqual(19, broker.cur_trade.stop_loss_price)
+        self.assertEqual(21, broker.cur_trade.take_profit_price)
 
 
 if __name__ == '__main__':
