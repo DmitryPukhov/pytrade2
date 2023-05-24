1e53a5361000151954e7c17ecc69ea7e353dc456
Dmitry Pukhov <pukhov@gnivc.ru>
2023-05-24 22:54:32 +0300

    lstm2 weights updated

--- biml/strategy/LSTMStrategy2.py
+++ biml/strategy/LSTMStrategy2.py
@@ -11,16 +11,17 @@ class LSTMStrategy2(LSTMStrategyBase):
 
     def create_model(self, X_size, y_size):
         model = Sequential()
-        # 10,825,588,190,59
-        model.add(LSTM(825, return_sequences=True, input_shape=(self.lstm_window_size, X_size)))
+        # LSTM2v1 was 10,825,588,190,59
+        # LSTM2v2 now
+        model.add(LSTM(320, return_sequences=True, input_shape=(self.lstm_window_size, X_size)))
 
-        model.add(Dropout(0.1))
-        model.add(LSTM(588))
         model.add(Dropout(0.2))
-        model.add(Dense(190, activation='relu'))
-        model.add(Dropout(0.1))
-        model.add(Dense(59, activation='relu'))
+        model.add(LSTM(160))
         model.add(Dropout(0.2))
+        model.add(Dense(40, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(16, activation='relu'))
+        model.add(Dropout(0.1))
         model.add(Dense(y_size, activation='linear')) # linear for regression
         model.compile(optimizer='adam', loss='mae', metrics=['mse'])
         # Load weights
