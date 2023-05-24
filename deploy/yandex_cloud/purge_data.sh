5593f0bb32e567fd59ddbfd37556dd6aaf832d19
Dmitry Pukhov <pukhov@gnivc.ru>
2023-05-24 10:12:59 +0300

    purge_data.sh script added

---
+++ deploy/yandex_cloud/purge_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#####################################################
+# Purge remote data directory, leave only last 2 days
+#####################################################
+
+public_ip=$(yc vpc address list | grep biml-trade-bots | awk '{print $6}')
+user="yc-user"
+data_dir="/home/$user/biml/data"
+
+
+echo "Purge data in $public_ip"
+
+ssh "$user@$public_ip" "cd biml/data; \
+ for strategy_dir in \$(ls); do \
+    strategy_data_dir=$data_dir/\$strategy_dir\/Xy;
+    cd \$strategy_data_dir;
+    last_file=\$(ls | sort | tail -n 1)
+    prefix=\${last_file:0:10}
+    echo \"Purging \$strategy_data_dir. Leave only \$prefix \"
+    sudo find . ! -name \"\${prefix}*\" -type f -delete
+ done"
+
