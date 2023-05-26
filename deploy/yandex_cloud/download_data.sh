43757a3db352cbc33893e0daeff8379cf5d4692e
Dmitry Pukhov <pukhov@gnivc.ru>
2023-05-26 07:43:16 +0300

    download_data.sh updated to print last trades times

--- deploy/yandex_cloud/download_data.sh
+++ deploy/yandex_cloud/download_data.sh
@@ -4,9 +4,27 @@ public_ip="$(yc compute instance list | grep biml-trade-bots | awk '{print $10}'
 user="yc-user"
 local_biml_dir="$(pwd)/../.."
 local_data_dir="$local_biml_dir/data/yandex-cloud"
-mkdir -p $local_data_dir
-vm_data_dir="/home/$user/biml/data"
-echo "Downloading bots data from $public_ip to $local_data_dir"
-cmd="rsync -v -r $user@$public_ip:$vm_data_dir/  $local_data_dir"
-echo $cmd
-$cmd
+
+# Download from remote to local folder
+download_data(){
+  mkdir -p $local_data_dir
+  vm_data_dir="/home/$user/biml/data"
+  echo "Downloading bots data from $public_ip to $local_data_dir"
+  rsync -v -r $user@$public_ip:$vm_data_dir/  $local_data_dir
+}
+
+# Print strategies trades info
+print_last_trades(){
+  for strategy_dir in $local_data_dir/*
+  do
+    strategy="$(basename -- $strategy_dir)"
+    strategy_file="$strategy_dir/$strategy.db"
+    last_close_time=$(sqlite3 "$strategy_file" "SELECT MAX(close_time) from trade")
+    last_open_time=$(sqlite3 "$strategy_file" "SELECT MAX(open_time) from trade")
+    echo "$strategy last opened: $last_open_time, closed: $last_close_time"
+  done
+}
+
+
+download_data
+print_last_trades
