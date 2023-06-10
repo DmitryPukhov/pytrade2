5b72fdba9f9b797441f13d9acd57b1d7ed9bcd3a
Dmitry Pukhov <pukhov@gnivc.ru>
2023-06-11 01:36:33 +0300

    purge_data updated to purge account directories

--- deploy/yandex_cloud/purge_data.sh
+++ deploy/yandex_cloud/purge_data.sh
@@ -13,11 +13,13 @@ echo "Purge data in $public_ip"
 
 ssh "$user@$public_ip" "cd pytrade2/data; \
  for strategy_dir in \$(ls); do \
-    strategy_data_dir=$data_dir/\$strategy_dir\/Xy;
-    cd \$strategy_data_dir;
-    last_file=\$(ls | sort | tail -n 1)
-    prefix=\${last_file:0:10}
-    echo \"Purging \$strategy_data_dir. Leave only \$prefix \"
-    sudo find . ! -name \"\${prefix}*\" -type f -delete
+    for subdir in Xy account; do \
+      strategy_data_dir=$data_dir/\$strategy_dir\/\$subdir;
+      cd \$strategy_data_dir;
+      last_file=\$(ls | sort | tail -n 1)
+      prefix=\${last_file:0:10}
+      echo \"Purging \$strategy_data_dir Leave only \$prefix \"
+      sudo find . ! -name \"\${prefix}*\" -type f -delete
+    done;
  done"
 
