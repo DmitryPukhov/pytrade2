e8caee1b2242cfd02904363f194e2b544a8b5666
Dmitry Pukhov <pukhov@gnivc.ru>
2023-06-19 14:25:58 +0300

    reconfigure.sh improved to reconfigure and restart all containers

--- deploy/yandex_cloud/reconfigure.sh
+++ deploy/yandex_cloud/reconfigure.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-bot_name=${1?"Please provide bot name. Example: $0 lstmstrategy"} # arg1 is a bot name
-
-local_cfg_dir="$(pwd)/secret"
-local_cfg="$local_cfg_dir/$bot_name.yaml"
-local_log_cfg="$local_cfg_dir/log.cfg"
-tmp_cfg_dir="$(pwd)/tmp/pytrade2/pytrade2/cfg"
-tmp_cfg="$tmp_cfg_dir/$bot_name.yaml"
-tmp_log_cfg="$tmp_cfg_dir/log.cfg"
-
+default_bot_names="lstmstrategy2 lstmstrategy simplekerasstrategy"
+bot_names=${*:-"$default_bot_names"}
 
 user="yc-user"
 public_ip="$(yc compute instance list | grep pytrade2 | awk '{print $10}')"
+
+
+local_cfg_dir="$(pwd)/secret"
+tmp_cfg_dir="$(pwd)/tmp/pytrade2/pytrade2/cfg"
+user="yc-user"
 pytrade2_vm_dir="/home/$user/pytrade2"
-remote_cfg="$user@$public_ip:$pytrade2_vm_dir/pytrade2/cfg/$bot_name.yaml"
-remote_log_cfg="$user@$public_ip:$pytrade2_vm_dir/pytrade2/cfg/log.cfg"
+remote_cfg_dir="$user@$public_ip:$pytrade2_vm_dir/pytrade2/cfg"
+
 
 
 echo "Reconfiguring $bot_name at $public_ip machine. Copy $local_cfg to $remote_cfg"
 
-echo "Stopping remote docker"
-ssh "$user@$public_ip" "cd $pytrade2_vm_dir ; sudo docker-compose stop $bot_name"
 
-echo "Copy new configs to remote"
-cp -f "$local_cfg" "$tmp_cfg"
-scp "$tmp_cfg" "$remote_cfg"
-
-echo "Copy new log cfg to remote"
-cp -f "$local_log_cfg" "$tmp_log_cfg"
-scp "$tmp_log_cfg" "$remote_log_cfg"
+copy_configs() {
+  echo "Copy new configs to tmp"
+  for f in "$local_cfg_dir"/*.yaml; do cp -fv "$f" "$tmp_cfg_dir"; done
+  for f in "$local_cfg_dir"/*.cfg; do cp -fv "$f" "$tmp_cfg_dir"; done
+  echo "Copy $tmp_cfg_dir to $remote_cfg_dir"
+  rsync -vr "$tmp_cfg_dir/" "$remote_cfg_dir/"
+}
 
 
-echo "Starting remote docker"
-ssh $user@"$public_ip" "cd $pytrade2_vm_dir ; sudo docker-compose start $bot_name &"
+####### main #######
+echo "Stopping remote dockers"
+ssh "$user@$public_ip" "cd $pytrade2_vm_dir ; sudo docker-compose stop $bot_names"
 
-./logs.sh $bot_name
+copy_configs
+
+echo "Starting remote dockers"
+ssh $user@"$public_ip" "cd $pytrade2_vm_dir ; sudo docker-compose start $bot_names &"
+
+## Follow logs
+ssh $user@"$public_ip" "cd $pytrade2_vm_dir ; sudo docker-compose logs -f"
