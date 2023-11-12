01bc7617df31f7f60b57a90c723af94e1ce10627
dmitriy.pukhov <pukhov@gnivc.ru>
2023-11-12 10:33:19 +0300

    reconfigure.sh fixed to work with multiple containers

--- deploy/yandex_cloud/reconfigure.sh
+++ deploy/yandex_cloud/reconfigure.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-bot_names=${*:-"$DEFAULT_BOT_NAMES"}
 
 . deploy_lib.sh
+bot_names=${*:-"$DEFAULT_BOT_NAMES"}
 
 
 echo "Reconfiguring $bot_names at $VM_PUBLIC_IP machine."
 
 ####### main #######
-echo "Stopping remote dockers"
-ssh "$VM_USER@$VM_PUBLIC_IP" "cd $VM_PYTRADE2_DIR ; sudo docker-compose stop $bot_names"
+echo "Stopping remote dockers of $bot_names"
+ssh "$VM_USER@$VM_PUBLIC_IP" "cd $VM_PYTRADE2_DIR ; sudo docker stop $bot_names"
 
 prepare_tmp_config
 copy_to_remote_config
 
-echo "Starting remote dockers"
-ssh "$VM_USER@$VM_PUBLIC_IP" "cd $VM_PYTRADE2_DIR ; sudo docker-compose start $bot_names &"
+echo "Starting remote dockers or $bot_names"
+ssh "$VM_USER@$VM_PUBLIC_IP" "cd $VM_PYTRADE2_DIR ; sudo docker-compose up $bot_names &"
 
 ## Follow logs
 ssh "$VM_USER@$VM_PUBLIC_IP" "cd $VM_PYTRADE2_DIR ; sudo docker-compose logs -n100 -f"
