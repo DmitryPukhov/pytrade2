12b8267b5f32ce30cf0666d75acd1a72374279ce
dmitriy.pukhov <pukhov@gnivc.ru>
2023-12-14 12:26:42 +0300

    infra virtual machine switched to python3.11

--- deploy/yandex_cloud/vm_create.sh
+++ deploy/yandex_cloud/vm_create.sh
@@ -8,8 +8,7 @@ create_instance() {
   yc compute instance create \
     --name $app_name \
     --hostname $app_name \
-    --zone $zone\
-    --memory 4 \
+    --zone $zone --memory 4 \
     --cores 2 \
     --platform "standard-v3" \
     --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4,nat-address="$public_ip" \
@@ -20,26 +19,25 @@ create_instance() {
 install_prereq() {
   user="yc-user"
   echo "Install prerequisites to $public_ip"
-  ssh "$user"@"$public_ip" "sudo snap install python38"
-  ssh $user@"$public_ip" "sudo snap install yes"
-  ssh "$user"@"$public_ip" "sudo snap refresh snapd"
-  ssh "$user"@"$public_ip" "sudo snap install docker"
-}
-
-install_baremetal() {
-  ssh "$user"@"$public_ip" "sudo snap install python38"
+  ssh "$user@$public_ip" "sudo add-apt-repository -y ppa:deadsnakes/ppa && sudo apt update && sudo apt install -y python3.11"
+  ssh "$user@$public_ip" "sudo snap install yes"
+  ssh "$user@$public_ip" "sudo snap refresh snapd"
+  ssh "$user@$public_ip" "sudo snap install docker"
 }
 
 # Main
 
 # Static ip
-yc vpc address create --name $app_name --external-ipv4 zone=$zone
+if [[ "$*" == *"--new-public-ip"* ]]; then
+  echo "Creating new public ip"
+  yc vpc address create --name $app_name --external-ipv4 zone=$zone
+fi
+
 public_ip=$(yc vpc address list | grep $app_name | awk '{print $6}')
+
 
 # Vm
 create_instance
 
 # Docker
 install_prereq
-
-
--- deploy/yandex_cloud/vm_delete.sh
+++ deploy/yandex_cloud/vm_delete.sh
@@ -7,5 +7,8 @@ yc compute instance stop --name $app_name || true
 echo "Deleting pytrade2 vm $app_name"
 yc compute instance delete --name $app_name
 
-echo "Deleting static ip for $app_name"
-yc vpc address delete $app_name
+
+if [[ "$*" == *"--delete-public-ip"* ]]; then
+  echo "Deleting static ip for $app_name"
+  yc vpc address delete $app_name
+fi
