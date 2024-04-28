a8402fabf987219fd7af6b6e6bb93a3aaf065da7
dmitriy.pukhov <pukhov@gnivc.ru>
2024-04-28 11:37:44 +0300

    Grafana dashboard added

--- pytrade2/metrics/Metrics.py
+++ pytrade2/metrics/Metrics.py
@@ -56,7 +56,7 @@ class Metrics:
 
     @classmethod
     def name_of(cls, source, suffix):
-        return f'pytrade2_{Metrics.app_name}_{source.__class__.__name__.lower()}_{suffix}'
+        return f'pytrade2_{Metrics.app_name.lower()}_{source.__class__.__name__.lower()}_{suffix}'
 
     @classmethod
     def gauge(cls, source, suffix) -> Gauge:
