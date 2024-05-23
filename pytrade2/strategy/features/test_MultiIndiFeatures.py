e78103300fa0e2442943ad5c54e6b18fe3b231e1
dmitriy.pukhov <pukhov@gnivc.ru>
2024-05-23 19:50:45 +0300

    MultiInidiFeatures - fixed unit test

--- pytrade2/strategy/features/test_MultiIndiFeatures.py
+++ pytrade2/strategy/features/test_MultiIndiFeatures.py
@@ -6,11 +6,11 @@ from strategy.features.MultiIndiFeatures import MultiIndiFeatures
 
 
 class TestMultiIndiFeatures(TestCase):
-    def test_multi_indi_features_last_same_candle_should_not_produce_empty_features(self):
+    def test_multi_indi_features_last_same_candle_should_produce_empty_features(self):
         candles = map(
             lambda x: {"datetime": datetime(year=2024, month=9, day=10) + timedelta(minutes=x), "open": 0, "high": 0, "low": 0,
                        "close": 0, "vol":0}, range(60))
         candles = pd.DataFrame(candles).set_index("datetime")
 
         actual =  MultiIndiFeatures.multi_indi_features({"1min": candles})
-        self.assertFalse(actual.empty)
+        self.assertTrue(actual.empty)
