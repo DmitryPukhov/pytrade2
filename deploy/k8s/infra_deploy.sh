dbaba8293bc6ebf99c6ac69a465b8a3c39a257cb
dmitriy.pukhov <pukhov@gnivc.ru>
2024-08-03 10:05:30 +0300

    mlflow deployment cleaned up

--- deploy/k8s/infra_deploy.sh
+++ deploy/k8s/infra_deploy.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 
+# All secrets
 function redeploy_secrets(){
   for yaml_file in secret/*.yaml; do
       echo "Deploying secret $yaml_file"
       secret_name=$(basename "${yaml_file%.*}")
       kubectl delete secret "$secret_name"
+      # Deploy the secret
       kubectl apply -f "$yaml_file"
   done
 }
 
+# Mlflow run, tracking, postgresql
 function redeploy_mlflow(){
   echo "Redeploying mlflow"
+  # Delete previous ignoring errors if not exists
+  set +e
   helm delete pytrade2-mlflow
   kubectl delete pvc data-pytrade2-mlflow-postgresql-0
+  kubectl delete pvc pytrade2-mlflow-tracking
+  set -e
 
-  # pvc
-  #kubectl delete pvc pytrade2-mlflow-tracking
-  #kubectl get pvc
-
+  # Install mlflow
   helm install pytrade2-mlflow oci://registry-1.docker.io/bitnamicharts/mlflow --values mlflow/values.yaml
 }
 
+# Grafana
 function redeploy_grafana(){
   echo "Redeploying grafana"
+  # Delete previous ignoring errors if not exists
+  set +e
   helm delete pytrade2-grafana
   kubectl delete pvc pytrade2-grafana
+  set -e
+
+  # install grafana
   helm install pytrade2-grafana oci://registry-1.docker.io/bitnamicharts/grafana --values grafana/values.yaml
 }
 
+###############
+# main
+###############
+# Exit on error
+set -e
+
 redeploy_secrets
-#redeploy_mlflow
+redeploy_mlflow
 redeploy_grafana
