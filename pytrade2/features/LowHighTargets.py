33d687fe2293cdfec8cc7ba501e6c93fda9d2789
dmitriy.pukhov <pukhov@gnivc.ru>
2025-05-10 16:46:13 +0300

    LowHighTargets - added classification targets

--- pytrade2/features/LowHighTargets.py
+++ pytrade2/features/LowHighTargets.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -6,9 +7,39 @@ class LowHighTargets:
     def fut_lohi(candles, predict_window: str):
         fut_lohi = pd.DataFrame(index=candles.index)
         fut_lohi[['fut_low', 'fut_high']] = (candles[['low', 'high']][::-1]
-                                             .rolling(predict_window, closed='both').agg({'low': 'min', 'high': 'max'})[::-1])
-        fut_lohi['fut_low_diff'] = fut_lohi['fut_low'] - candles['close']
-        fut_lohi['fut_high_diff'] = fut_lohi['fut_high'] - candles['close']
+                                             .rolling(predict_window, closed='both').agg({'low': 'min', 'high': 'max'})[
+                                             ::-1])
+        fut_lohi['fut_low_delta'] = fut_lohi['fut_low'] - candles['close']
+        fut_lohi['fut_high_delta'] = fut_lohi['fut_high'] - candles['close']
         # Drop last data with unfinished prediction windows
         fut_lohi = fut_lohi.iloc[fut_lohi.index <= max(fut_lohi.index) - pd.Timedelta(predict_window)]
-        return fut_lohi[['fut_low_diff', 'fut_high_diff']]
+        return fut_lohi[['fut_low_delta', 'fut_high_delta']]
+
+    @staticmethod
+    def fut_lohi_signal_target(df_candles_1_min: pd.DataFrame,
+                               predict_window: str,
+                               loss_coeff: float,
+                               profit_loss_ratio: float
+                               ):
+        # Calc low, high for future
+        df = df_candles_1_min.copy()
+        df[['fut_low', 'fut_high']] = df_candles_1_min[['low', 'high']][::-1] \
+                                          .rolling(predict_window, closed='left') \
+                                          .agg({'low': 'min', 'high': 'max'})[::-1]
+
+        fut_low_delta = df["fut_low"] - df["close"]
+        fut_high_delta = df["fut_high"] - df["close"]
+        max_loss = df["close"] * loss_coeff
+        min_profit = max_loss * profit_loss_ratio
+        direction = np.where(-fut_low_delta > fut_high_delta, -1,
+                             np.where(fut_low_delta < fut_high_delta, 1, 0)
+                             )  # 1 - buy, -1 - sell
+        loss_ok = np.where(direction > 0,
+                           abs(fut_low_delta) < max_loss, # Trend up, low delta is loss
+                           abs(fut_high_delta) < max_loss) # Trend down, high delta is loss
+        profit_ok = np.where(direction > 0,
+                             fut_high_delta >= min_profit, # Trend up, high delta is profit
+                             abs(fut_low_delta) >= min_profit) # Trend down, low delta is profit
+        df["signal"] = direction * (loss_ok & profit_ok).astype(int)
+
+        return df[["signal"]]
--- pytrade2/features/test_LowHighTargets.py
+++ pytrade2/features/test_LowHighTargets.py
@@ -7,6 +7,69 @@ from features.LowHighTargets import LowHighTargets
 
 
 class TestLowHighTargets(TestCase):
+    def test_fut_lohi_signal_target_buy(self):
+        candles = pd.DataFrame([
+            {'datetime': datetime.fromisoformat('2024-02-07 20:01'), 'close': 100, 'low': 1000, 'high': -1000},
+            {'datetime': datetime.fromisoformat('2024-02-07 20:02'), 'close': 100, 'low': 99.1, 'high': 104}
+        ]).set_index('datetime')
+
+        actual = LowHighTargets.fut_lohi_signal_target(candles, '1min', 0.01, 4)
+        self.assertSequenceEqual(candles.index.tolist(), actual.index.tolist())
+        self.assertSequenceEqual([1,0],actual["signal"].tolist())
+
+    def test_fut_lohi_signal_small_profit_not_buy(self):
+        # Profit is too small
+        candles = pd.DataFrame([
+            {'datetime': datetime.fromisoformat('2024-02-07 20:01'), 'close': 100, 'low': 1000, 'high': -1000},
+            {'datetime': datetime.fromisoformat('2024-02-07 20:02'), 'close': 100, 'low': 99.1, 'high': 103.9}
+        ]).set_index('datetime')
+
+        actual = LowHighTargets.fut_lohi_signal_target(candles, '1min', 0.01, 4)
+        self.assertSequenceEqual(candles.index.tolist(), actual.index.tolist())
+        self.assertSequenceEqual([0,0],actual["signal"].tolist())
+
+    def test_fut_lohi_signal_target_big_loss_not_buy(self):
+        # Los is too big
+        candles = pd.DataFrame([
+            {'datetime': datetime.fromisoformat('2024-02-07 20:01'), 'close': 100, 'low': 1000, 'high': -1000},
+            {'datetime': datetime.fromisoformat('2024-02-07 20:02'), 'close': 100, 'low': 99, 'high': 104}
+        ]).set_index('datetime')
+
+        actual = LowHighTargets.fut_lohi_signal_target(candles, '1min', 0.01, 4)
+        self.assertSequenceEqual(candles.index.tolist(), actual.index.tolist())
+        self.assertSequenceEqual([0,0],actual["signal"].tolist())
+
+    def test_fut_lohi_signal_target_sell(self):
+        candles = pd.DataFrame([
+            {'datetime': datetime.fromisoformat('2024-02-07 20:01'), 'close': 100, 'low': 1000, 'high': -1000},
+            {'datetime': datetime.fromisoformat('2024-02-07 20:02'), 'close': 100, 'low': 96, 'high': 100.9}
+        ]).set_index('datetime')
+
+        actual = LowHighTargets.fut_lohi_signal_target(candles, '1min', 0.01, 4)
+        self.assertSequenceEqual(candles.index.tolist(), actual.index.tolist())
+        self.assertSequenceEqual([-1,0],actual["signal"].tolist())
+
+    def test_fut_lohi_signal_target_small_profit_not_sell(self):
+        candles = pd.DataFrame([
+            {'datetime': datetime.fromisoformat('2024-02-07 20:01'), 'close': 100, 'low': 1000, 'high': -1000},
+            {'datetime': datetime.fromisoformat('2024-02-07 20:02'), 'close': 100, 'low': 96.1, 'high': 100.9}
+        ]).set_index('datetime')
+
+        actual = LowHighTargets.fut_lohi_signal_target(candles, '1min', 0.01, 4)
+        self.assertSequenceEqual(candles.index.tolist(), actual.index.tolist())
+        self.assertSequenceEqual([0,0],actual["signal"].tolist())
+
+
+    def test_fut_lohi_signal_target_big_loss_not_sell(self):
+        candles = pd.DataFrame([
+            {'datetime': datetime.fromisoformat('2024-02-07 20:01'), 'close': 100, 'low': 1000, 'high': -1000},
+            {'datetime': datetime.fromisoformat('2024-02-07 20:02'), 'close': 100, 'low': 96, 'high': 101}
+        ]).set_index('datetime')
+
+        actual = LowHighTargets.fut_lohi_signal_target(candles, '1min', 0.01, 4)
+        self.assertSequenceEqual(candles.index.tolist(), actual.index.tolist())
+        self.assertSequenceEqual([0,0],actual["signal"].tolist())
+
     def test_fut_lohi(self):
         candles = pd.DataFrame([
             {'datetime': datetime.fromisoformat('2024-02-07 20:01'), 'close': 0, 'low': -1, 'high': 1},
@@ -17,5 +80,5 @@ class TestLowHighTargets(TestCase):
 
         actual = LowHighTargets.fut_lohi(candles, '1min')
         self.assertSequenceEqual(candles.index[:-1].tolist(), actual.index.tolist())
-        self.assertSequenceEqual([-2, -4, -7], actual['fut_low_diff'].tolist())
-        self.assertSequenceEqual([2, 4, 7], actual['fut_high_diff'].tolist())
+        self.assertSequenceEqual([-2, -4, -7], actual['fut_low_delta'].tolist())
+        self.assertSequenceEqual([2, 4, 7], actual['fut_high_delta'].tolist())
