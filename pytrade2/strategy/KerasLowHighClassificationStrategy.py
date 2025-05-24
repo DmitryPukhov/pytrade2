562097eb6eee3c30f451ca21c849ead080d19f64
dmitriy.pukhov <pukhov@gnivc.ru>
2025-05-24 15:01:43 +0300

    SignalClassificationStrategyBase.py removed, methods moved to single child KerasLowHighClassificationStrategy

--- pytrade2/strategy/KerasLowHighClassificationStrategy.py
+++ pytrade2/strategy/KerasLowHighClassificationStrategy.py
@@ -1,22 +1,20 @@
-import logging
+#from html.parser import starttagopen
 from typing import Dict
 
 import pandas as pd
 from keras import Sequential, Input
 from keras.layers import Dense, Dropout
-from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import RobustScaler, MinMaxScaler, OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
 
 from exch.Exchange import Exchange
-from strategy.common.LearnDataBalancer import LearnDataBalancer
-from strategy.common.SignalClassificationStrategyBase import SignalClassificationStrategyBase
-from strategy.common.StrategyBase import StrategyBase
 from features.LongCandleFeatures import LongCandleFeatures
+from strategy.common.LearnDataBalancer import LearnDataBalancer
+from strategy.common.StrategyBase import StrategyBase
 from strategy.signal.OrderParamsByLastCandle import OrderParamsByLastCandle
 
 
-class KerasLowHighClassificationStrategy(SignalClassificationStrategyBase):
+class KerasLowHighClassificationStrategy(StrategyBase):
     """
      Predict long candle signal, classification model, target signals: -1, 0, 1
    """
@@ -93,3 +91,12 @@ class KerasLowHighClassificationStrategy(SignalClassificationStrategyBase):
         model.summary()
         return model
 
+    def create_pipe(self, X, y) -> (Pipeline, Pipeline):
+        """ Create feature and target pipelines to use for transform and inverse transform """
+        x_pipe, _ = super().create_pipe(X, y)
+
+        # One hot encode y
+        y_pipe = Pipeline([('adjust_labels', OneHotEncoder(categories=[[-1, 0, 1]], sparse_output=False, drop=None))])
+        y_pipe.fit(y)
+
+        return x_pipe, y_pipe
