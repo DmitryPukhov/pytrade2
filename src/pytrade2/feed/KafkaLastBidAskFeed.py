b593de900a2e345eecaa8bd5a4949df51ae0e26e
dmitriy.pukhov <pukhov@gnivc.ru>
2025-06-25 15:25:58 +0300

    KafkaLastBidAskFeed fixed

--- src/pytrade2/feed/KafkaLastBidAskFeed.py
+++ src/pytrade2/feed/KafkaLastBidAskFeed.py
@@ -1,15 +1,18 @@
 import multiprocessing
 
+from pytrade2.feed.KafkaFeedBase import KafkaFeedBase
 
-class KafkaLastBidAskFeed:
+
+class KafkaLastBidAskFeed(KafkaFeedBase):
     """ Decorator for strategies. Reads candles from exchange """
 
     kind = "bidask"
 
     def __init__(self, config: dict[str, str], data_lock: multiprocessing.RLock,
                  new_data_event: multiprocessing.Event):
-        self._features_topic = config['pytrade2.feed.bid_ask.kafka.topic']
-        self._data_lock = data_lock
+        self._bid_ask_topic = config['pytrade2.feed.bid_ask.kafka.topic']
+        super().__init__(self._bid_ask_topic, config, data_lock, new_data_event)
+
         self.last_bid_ask = None
 
     def apply_buf(self):
@@ -17,6 +20,6 @@ class KafkaLastBidAskFeed:
         ...
 
     def on_message(self, message):
-        with self._data_lock:
+        with self.data_lock:
             self.last_bid_ask = message
 
